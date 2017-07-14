2ae9a28d011a4592d5f72c5db9e7665a7eddae11
Olga Filipova <chudaol@gmail.com>
2017-07-14 16:19:33 +0200

    removed logo and tagline

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,7 +1,5 @@
 <template>
   <div>
-    <logo></logo>
-    <tagline></tagline>
     <div class="container row justify-content-center align-items-center">
       <div class="col-sm-12 col-md-6 col-lg-6">
         <authentication></authentication>
