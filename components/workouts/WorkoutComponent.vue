50b0b7a268f93cce1edb2982c0777097086ab692
Olga Filipova <chudaol@gmail.com>
2017-08-09 21:35:38 +0200

    small adjustment in the modal

--- components/workouts/WorkoutComponent.vue
+++ components/workouts/WorkoutComponent.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="modal fade" id="workoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal fade" id="workoutModal" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">{{ name }}</h5>
+          <h5 class="modal-title">{{ name }}</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
