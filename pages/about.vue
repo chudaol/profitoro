4d623ecb37166be578d4c8a6b87e9f7b0a4a046e
Olga Filipova <chudaol@gmail.com>
2017-08-22 22:34:23 +0200

    edited the about page

--- pages/about.vue
+++ pages/about.vue
@@ -3,7 +3,7 @@
     <header-component></header-component>
     <div class="container min-full-height">
       <p class="lead">
-        ProFitOro application is a time management application that combines the <a href="https://en.wikipedia.org/wiki/Pomodoro_Technique">Pomodoro technique</a> with office workouts.
+        ProFitOro is a time management application that combines the <a target="_blank" href="https://en.wikipedia.org/wiki/Pomodoro_Technique">Pomodoro technique</a> with office workouts.
       </p>
       <p class="lead">
         <nuxt-link to="login">Click here to login </nuxt-link> or just <nuxt-link to="pomodoro">start using ProFitOro </nuxt-link>without registration.
@@ -23,15 +23,15 @@
       </p>
       <h4 class="title">Book</h4>
       <p>
-        This application has been developed as a part of the book <a href="https://www.packtpub.com/web-development/web-development-bootstrap-and-vuejs">"Web development using Vue.js, Bootstrap and Firebase"</a>. It is an <a
+        This application has been developed as a part of the book <a target="_blank" href="https://www.packtpub.com/web-development/web-development-bootstrap-and-vuejs">"Web development using Vue.js, Bootstrap and Firebase"</a>. It is an <a
         href="https://github.com/chudaol/profitoro">open source project</a> licensed under the Apache License, Version 2.0.
       </p>
       <h4 class="title">Credits</h4>
       <p>Logo creation: <a href="mailto:car.marg@gmail.com">Carina</a></p>
-      <p>Mockups implementation: <a href="https://github.com/Safure">Safi</a></p>
+      <p>Mockups by: <a href="https://github.com/Safure">Safi</a></p>
       <p>Design and illustrations: <a href="https://www.behance.net/MeegsyWeegsy">Vanessa</a></p>
-      <p>Transforming design into scss and helping with implementation: <a href="https://github.com/fil090302">Filipe</a></p>
-      <p>Implementation using Vue, Boostrap and Firebase: <a href="https://github.com/chudaol">Olga</a></p>
+      <p>Design implementation: <a href="https://github.com/fil090302">Filipe</a></p>
+      <p>Functionality implementation using Vue, Bootstrap and Firebase: <a href="https://github.com/chudaol">Olga</a></p>
     </div>
     <footer-component></footer-component>
   </div>
