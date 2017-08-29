d7163df9fcaba863a1ad33391a271e49d35a70bb
Olga Filipova <chudaol@gmail.com>
2017-08-29 17:05:37 +0300

    added og properties

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,10 +9,10 @@ module.exports = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'ProFitOro is a Pomodoro timer implementation combined with office workouts. Take breaks during work. Exercise during breaks ' },
       { hid: 'keywords', name: 'keywords', content: 'Pomodoro, workouts, fit, time management, office workouts, pomodoro technique' },
-      { hid: 'image', name: 'og:image', content: 'https://firebasestorage.googleapis.com/v0/b/profitoro-ad0f0.appspot.com/o/assets%2Fsharing.png?alt=media&token=d8687d98-24a7-4a8a-b0c4-06a39b127443' },
-      { hid: 'url', name: 'og:url', content: 'https://www.profitorolife.com' },
-      { hid: 'title', name: 'og:title', content: 'ProFitOro' },
-      { hid: 'ogdescription', name: 'og:description', content: 'ProFitOro is a Pomodoro timer implementation combined with office workouts. Take breaks during work. Exercise during breaks ' }
+      { hid: 'image', property: 'og:image', name: 'og:image', content: 'https://firebasestorage.googleapis.com/v0/b/profitoro-ad0f0.appspot.com/o/assets%2Fsharing.png?alt=media&token=d8687d98-24a7-4a8a-b0c4-06a39b127443' },
+      { hid: 'url', property: 'og:url', name: 'og:url', content: 'https://www.profitorolife.com' },
+      { hid: 'title', property: 'og:title', name: 'og:title', content: 'ProFitOro' },
+      { hid: 'ogdescription', property: 'og:description', name: 'og:description', content: 'ProFitOro is a Pomodoro timer implementation combined with office workouts. Take breaks during work. Exercise during breaks ' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
